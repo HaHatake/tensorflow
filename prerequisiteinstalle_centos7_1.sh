7036e25557b6151a8312607bc35998f6a2e286bb
HaHatake <hideahat@gmail.com>
2018-07-20 10:33:22 +0900

    Update prerequisiteinstalle_centos7_1.sh

--- prerequisiteinstalle_centos7_1.sh
+++ prerequisiteinstalle_centos7_1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo yum -y install gcc wget curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-ExtUtils-MakeMaker autoconf bzip2 bzip2-devel libbz2-dev openssl openssl-devel readline readline-devel
-curl -LO 
+curl -LO https://raw.githubusercontent.com/HaHatake/tensorflow/master/prerequisiteinstalle_centos7_2.sh
+sudo sh prerequisiteinstalle_centos7_2.sh
